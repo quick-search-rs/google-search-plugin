a98c600d812d2da8fbd90182a850e0acb762c30a
Dissy <you@willsh.art>
2024-02-21 03:24:01 -0500

    searching google and stuff

---
+++ src/lib.rs
@@ -0,0 +1,103 @@
+use abi_stable::{
+    export_root_module,
+    prefix_type::PrefixTypeTrait,
+    sabi_extern_fn,
+    sabi_trait::prelude::TD_Opaque,
+    std_types::{RBox, RStr, RString, RVec},
+};
+use quick_search_lib::{ColoredChar, PluginId, SearchLib, SearchLib_Ref, SearchResult, Searchable, Searchable_TO};
+
+static NAME: &str = "Google-Search";
+
+#[export_root_module]
+pub fn get_library() -> SearchLib_Ref {
+    SearchLib { get_searchable }.leak_into_prefix()
+}
+
+#[sabi_extern_fn]
+fn get_searchable(id: PluginId) -> Searchable_TO<'static, RBox<()>> {
+    let this = Google::new(id);
+    Searchable_TO::from_value(this, TD_Opaque)
+}
+
+#[derive(Debug, Clone)]
+struct Google {
+    id: PluginId,
+    client: reqwest::blocking::Client,
+}
+
+impl Google {
+    fn new(id: PluginId) -> Self {
+        Self {
+            id,
+            client: reqwest::blocking::Client::new(),
+        }
+    }
+    fn get_individual_query(&self, query: String) -> SearchResult {
+        SearchResult::new(&query)
+    }
+}
+
+impl Searchable for Google {
+    fn search(&self, query: RString) -> RVec<SearchResult> {
+        let mut res: Vec<SearchResult> = vec![];
+
+        let url = format!("https://google.com/complete/search?q={}&output=toolbar&hl=en", urlencoding::encode(query.as_str()));
+
+        if let Ok(response) = self.client.get(url).send() {
+            if let Ok(text) = response.text() {
+                if let Ok(xml) = roxmltree::Document::parse(&text) {
+                    for node in xml.descendants() {
+                        if node.tag_name().name() == "suggestion" {
+                            if let Some(data) = node.attribute("data") {
+                                let data = data.to_string();
+                                res.push(self.get_individual_query(data));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        res.sort_by(|a, b| a.title().cmp(b.title()));
+        res.dedup_by(|a, b| a.title() == b.title());
+
+        res.into()
+    }
+    fn name(&self) -> RStr<'static> {
+        NAME.into()
+    }
+    fn colored_name(&self) -> RVec<quick_search_lib::ColoredChar> {
+        // can be dynamic although it's iffy how it might be used
+        vec![
+            ColoredChar::new_rgba('G', 66, 133, 244, 255),
+            ColoredChar::new_rgba('o', 219, 68, 55, 255),
+            ColoredChar::new_rgba('o', 244, 180, 0, 255),
+            ColoredChar::new_rgba('g', 66, 133, 244, 255),
+            ColoredChar::new_rgba('l', 15, 157, 88, 255),
+            ColoredChar::new_rgba('e', 219, 68, 55, 255),
+        ]
+        .into()
+    }
+    fn execute(&self, result: &SearchResult) {
+        // let s = result.extra_info();
+        // if let Ok::<clipboard::ClipboardContext, Box<dyn std::error::Error>>(mut clipboard) = clipboard::ClipboardProvider::new() {
+        //     if let Ok(()) = clipboard::ClipboardProvider::set_contents(&mut clipboard, s.to_owned()) {
+        //         println!("copied to clipboard: {}", s);
+        //     } else {
+        //         println!("failed to copy to clipboard: {}", s);
+        //     }
+        // } else {
+        //     log::error!("failed to copy to clipboard: {}", s);
+        // }
+
+        // finish up, above is a clipboard example
+
+        if let Err(e) = webbrowser::open(&format!("https://google.com/search?q={}", urlencoding::encode(result.title()))) {
+            log::error!("failed to open browser: {}", e);
+        }
+    }
+    fn plugin_id(&self) -> &PluginId {
+        &self.id
+    }
+}
